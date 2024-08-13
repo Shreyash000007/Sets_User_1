a0e471870d07ff17b265203b3f33f74b8ad76e91
Shreyash000007 <shreyasharg@gmail.com>
2024-08-13 17:20:33 +0530

    Shreyash

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,21 @@
-public class HashSets{
+package Sets_User_1;
+
+import java.util.*;
+
+public class HashSets
+{                                                                                               
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+public static void main(String[] args) 
+{
+    HashSet<Person> h = new HashSet<>();
+    h.add(new Person("Shreyash", 22));
+    h.add(new Person("Abhishek", 21));
+    h.add(new Person("Ratan", 24));
+    for (Person person : h) 
+    {
+       System.out.println(person);    
+    }
+    
+}
+  
 }
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,22 @@
+package Sets_User_1;
+import java.util.*;
 public class LinkedHashSets{
 public static void main(String[]args){
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+   LinkedHashSet <Object>h=new LinkedHashSet<>();
+   h.add(new Person("Shre", 22));
+   h.add(new Person("Abhi", 23));
+   h.add(new Person("Rat", 21));
+   Iterator <Object> it=h.iterator();
+   for (Object object : h) 
+   {
+     System.out.println(object);
+   }
+   System.out.println("---------------");
+   while (it.hasNext()) 
+   {
+      System.out.println(it.next()); 
+   }
+
+}
 }
